e112e78964133edf2b38a901538e5588982bd6bc
salmik11 <salmik11@gmail.com>
2025-09-16 15:48:32 +0200

    Update skript-xml-to-gh.py

--- skript-xml-to-gh.py
+++ skript-xml-to-gh.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 import time
 
 # --- Konfigurace ---
-url = "https://www.goldpc.cz/export/productsComplete.xml?patternId=-5&partnerId=3&hash=b6980705f27e462dc4b2da6beeb8f8b5b7746b5d927f5eaf5692ddc19d8cf3d0&exactCode=10749&stockState=1&defaultCategoryId=792"
+url = "https://www.goldpc.cz/export/productsComplete.xml?patternId=-5&partnerId=3&hash=b6980705f27e462dc4b2da6beeb8f8b5b7746b5d927f5eaf5692ddc19d8cf3d0&stockState=1&categoryId=1031&marginFrom=35"
 output_file = "gpcz_ntb.xml"
 
 headers = {
@@ -87,6 +87,7 @@ for shopitem in root.findall("SHOPITEM"):
 # --- Uložení vyčištěného feedu ---
 tree.write(output_file, encoding="utf-8", xml_declaration=True)
 print(f"Vyčištěný feed uložen do: {output_file}")
+
 
 
 
